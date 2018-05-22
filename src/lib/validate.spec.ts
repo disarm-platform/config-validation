e460f1ba12816f7c1c28c1d2d21d2bd1900ba2d0
Nicolai Davies <Nicolai.d@hotmail.com>
2018-05-22 14:44:30 +0200

    Testing some optional properties

--- src/lib/validate.spec.ts
+++ src/lib/validate.spec.ts
@@ -1,9 +1,9 @@
 // tslint:disable:no-expression-statement
 import {test} from 'ava'
 import { Response, Status } from './response';
-import { run_number_validation, number_config } from './validate'
+import { run_number_validation, number_config, string_config, run_optional_string_validation } from './validate'
 
-test('valid config', t => {
+test('valid config number', t => {
   const validConfig : number_config = {
     array_of_numbers: [1, 2],
     number: 1
@@ -12,9 +12,10 @@ test('valid config', t => {
   const result: Response = run_number_validation(validConfig)
 
   t.is(result.status, Status.Green)
+  t.is(result.messages.length, 0)
 })
 
-test('invalid config', t => {
+test('invalid config number', t => {
   const invalidConfig: number_config = {
     array_of_numbers: [1, 2],
     number: 4
@@ -23,4 +24,28 @@ test('invalid config', t => {
   const result: Response = run_number_validation(invalidConfig)
 
   t.is(result.status, Status.Yellow)
+  t.is(result.messages.length, 1)
+})
+
+test('valid config string', t => {
+  const validConfig: string_config = {
+    strings: ['hi', 'hello']
+  }
+
+  const result: Response = run_optional_string_validation(validConfig)
+
+  t.is(result.status, Status.Green)
+  t.is(result.messages.length, 0)
+})
+
+test('invalid config string', async t => {
+  const validConfig: string_config = {
+    strings: ['hi', 'hello'],
+    optional_string: 'not hello'
+  }
+
+  const result: Response = run_optional_string_validation(validConfig)
+
+  t.is(result.status, Status.Yellow)
+  t.is(result.messages.length, 1)
 })
--- src/lib/validate.ts
+++ src/lib/validate.ts
@@ -20,4 +20,31 @@ export function run_number_validation(config: number_config) : Response {
   }
 }
 
+export interface string_config {
+  strings: string[],
+  optional_string?: string
+}
 
+
+export function run_optional_string_validation(config: string_config) {
+  // validates that optional_string is in strings, if it exists
+
+  if (config.hasOwnProperty('optional_string')) {
+    if (config.strings.includes(config.optional_string as string)) {
+      return {
+        messages: [],
+        status: Status.Green
+      }
+    } else {
+      return {
+        messages: ['optional_string does not exist'],
+        status: Status.Yellow
+      }
+    }
+  } else {
+    return {
+      messages: [],
+      status: Status.Green
+    }
+  }
+}
