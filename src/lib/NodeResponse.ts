2b8213629fe2cf9671aa1ee67cf556ea37830e53
Nicolai Davies <Nicolai.d@hotmail.com>
2018-05-28 13:57:24 +0200

    Remove unused pieces of code

--- src/lib/NodeResponse.ts
+++
@@ -1,5 +0,0 @@
-export enum ENodeStatus {
-  Red = 'Red - fails internal validation',
-  Green = 'Green - passed internal validation',
-  Blue = 'Blue - node not required' // TODO: Is logically possible to not require a node you are testing?
-}
--- src/lib/validate.spec.ts
+++ src/lib/validate.spec.ts
@@ -1,79 +1,8 @@
 // tslint:disable:no-expression-statement
 import { test } from 'ava';
 import { HeightConstraint, WidthConstraint } from '../definitions/applets/irs_monitor';
-import { TNumberConfig, TStringConfig } from './Config';
-import { EEdgeStatus, TEdgeResponse } from './EdgeResponse';
 import { EUnifiedStatus, TUnifiedResponse } from './UnifiedResponse'
-import { runNumberValidation, runOptionalStringValidation, validate } from './validate';
-
-test('valid config number', t => {
-  const validConfig: TNumberConfig = {
-    array_of_numbers: [1, 2],
-    number: 1
-  };
-
-  const result: TEdgeResponse = runNumberValidation({
-    number_config: validConfig
-  });
-
-  t.is(result.status, EEdgeStatus.Green);
-  t.is(result.messages.length, 0);
-});
-
-test('invalid config number', t => {
-  const invalidConfig: TNumberConfig = {
-    array_of_numbers: [1, 2],
-    number: 4
-  };
-
-  const result: TEdgeResponse = runNumberValidation({
-    number_config: invalidConfig
-  });
-
-  t.is(result.status, EEdgeStatus.Yellow);
-  t.is(result.messages.length, 1);
-});
-
-test('valid config string', t => {
-  const validConfig: TStringConfig = {
-    strings: ['hi', 'hello']
-  };
-
-  const result: TEdgeResponse = runOptionalStringValidation({
-    string_config: validConfig
-  });
-
-  t.is(result.status, EEdgeStatus.Green);
-  t.is(result.messages.length, 0);
-});
-
-test('invalid config string', async t => {
-  const validConfig: TStringConfig = {
-    optional_string: 'not hello',
-    strings: ['hello', 'hi']
-  };
-
-  const result: TEdgeResponse = runOptionalStringValidation({
-    string_config: validConfig
-  });
-
-  t.is(result.status, EEdgeStatus.Yellow);
-  t.is(result.messages.length, 1);
-});
-
-test('internally invalid config string', async t => {
-  const internallyInvalidConfig = {
-    not_strings: ['hello', 'hi']
-  };
-  // @ts-ignore
-  const result: TEdgeResponse = runOptionalStringValidation({
-    string_config: internallyInvalidConfig
-  });
-
-  t.is(result.status, EEdgeStatus.Red);
-  t.is(result.messages.length, 1);
-});
-
+import {validate} from './validate'
 
 test('returns Green for a valid config', t => {
   // tslint:disable:object-literal-sort-keys
