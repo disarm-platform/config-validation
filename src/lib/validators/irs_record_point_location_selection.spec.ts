ebf2298d487597b10d38ab9b9066ec7bb8b924fd
Nicolai Davies <Nicolai.d@hotmail.com>
2018-05-28 13:10:13 +0200

    Irs record point & location selection

---
+++ src/lib/validators/irs_record_point_location_selection.spec.ts
@@ -0,0 +1,59 @@
+// tslint:disable:no-expression-statement
+import { test } from 'ava';
+import { EEdgeStatus } from '../EdgeResponse';
+import { irs_record_point_location_selection } from './irs_record_point_location_selection';
+
+test('returns Blue status if no irs_record_point', t => {
+  const config = {
+    applets: {}
+  }
+  // @ts-ignore
+  const result = irs_record_point_location_selection(config)
+
+  t.is(result.status, EEdgeStatus.Blue)
+  t.is(result.messages.length, 0)
+})
+
+test('returns Red status if no location_selection is missing', t => {
+  const config = {
+    applets: {
+      irs_record_point: {}
+    }
+  }
+  // @ts-ignore
+  const result = irs_record_point_location_selection(config)
+
+  t.is(result.status, EEdgeStatus.Red)
+  t.is(result.messages.length, 1)
+})
+
+
+test('returns Red status if location_selection is an object with no keys', t => {
+  const config = {
+    applets: {
+      irs_record_point: {}
+    },
+    location_selection: {}
+  }
+  // @ts-ignore
+  const result = irs_record_point_location_selection(config)
+
+  t.is(result.status, EEdgeStatus.Red)
+  t.is(result.messages.length, 1)
+})
+
+test('returns Green status if location_selection is there', t => {
+  const config = {
+    applets: {
+      irs_record_point: {}
+    },
+    location_selection: {
+      villages: []
+    }
+  }
+  // @ts-ignore
+  const result = irs_record_point_location_selection(config)
+
+  t.is(result.status, EEdgeStatus.Green)
+  t.is(result.messages.length, 0)
+})
---
+++ src/lib/validators/irs_record_point_location_selection.ts
@@ -0,0 +1,24 @@
+import { Config } from "../../definitions";
+import { EEdgeStatus, TEdgeResponse } from "../EdgeResponse";
+
+
+export function irs_record_point_location_selection(config: Config) : TEdgeResponse {
+  if (!config.applets.irs_record_point) {
+    return {
+      messages: [],
+      status: EEdgeStatus.Blue
+    }
+  }
+
+  if (!config.location_selection || Object.keys(config.location_selection).length === 0) {
+    return {
+      messages: [{description: 'Location selection missing, is required for irs_record_point.'}],
+      status: EEdgeStatus.Red
+    }
+  }
+
+  return {
+    messages: [],
+    status: EEdgeStatus.Green
+  }
+}
