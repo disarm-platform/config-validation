63945ff022b79fa7309a83155bbf28d9819e2786
Nicolai Davies <Nicolai.d@hotmail.com>
2018-05-29 13:27:38 +0200

    Validations and field helpers

--- src/lib/validators/irs_record_point_validations.spec.ts
+++ src/lib/validators/irs_record_point_validations.spec.ts
@@ -28,32 +28,10 @@ test('returns Blue status if no validations', t => {
   t.is(result.messages.length, 0)
 })
 
-test('returns Yellow status if validations expression form field is not in form', t => {
+test('returns Green if validations', t => {
   const config = {
     applets: {
       irs_record_point: {}
-    },
-    form: {
-      pages: [
-        {
-          "elements": [
-            {
-              "inputType": "number",
-              "isRequired": true,
-              "name": "number_sprayable",
-              "title": "How many sprayable structures in the household/homestead?",
-              "type": "text",
-              "validators": [
-                {
-                  "text": "Minimum Value is Zero",
-                  "type": "numeric",
-                }
-              ]
-            }
-          ],
-          "name": "page1"
-        }
-      ]
     },
     validations: [{
       "expression": "numbersprayed_ddt <= number_sprayable",
@@ -62,51 +40,11 @@ test('returns Yellow status if validations expression form field is not in form'
       "type": "error"
     }]
   }
-  // @ts-ignore
-  const result = irs_record_point_validations(config)
 
-  t.is(result.status, EEdgeStatus.Yellow)
-  t.is(result.messages.length, 1)
-})
-
-
-test('returns Green status if validations are valid', t => {
-  const config = {
-    applets: {
-      irs_record_point: {}
-    },
-    form: {
-      pages: [
-        {
-          "elements": [
-            {
-              "inputType": "number",
-              "isRequired": true,
-              "name": "number_sprayable",
-              "title": "How many sprayable structures in the household/homestead?",
-              "type": "text",
-              "validators": [
-                {
-                  "text": "Minimum Value is Zero",
-                  "type": "numeric",
-                }
-              ]
-            }
-          ],
-          "name": "page1"
-        }
-      ]
-    },
-    validations: [{
-      "expression": "number_sprayable > 0",
-      "message": "Sprayed more with DDT than total",
-      "name": "sprayed more with ddt than total",
-      "type": "error"
-    }]
-  }
   // @ts-ignore
   const result = irs_record_point_validations(config)
 
   t.is(result.status, EEdgeStatus.Green)
   t.is(result.messages.length, 0)
+  
 })
