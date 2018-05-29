2df759332363f5e3fe17416fddc6ab057a0a3d62
Nicolai Davies <Nicolai.d@hotmail.com>
2018-05-29 09:09:37 +0200

    Fix messages, so it’s easier to know where the error is

--- src/lib/validators/irs_monitor_aggregations.ts
+++ src/lib/validators/irs_monitor_aggregations.ts
@@ -1,4 +1,3 @@
-import { flatten } from 'lodash'
 import { Config } from "../../definitions";
 import { EEdgeStatus, TEdgeResponse } from "../EdgeResponse";
 
@@ -18,53 +17,64 @@ export function irs_monitor_aggregations(config: Config): TEdgeResponse {
     }
   }
 
-  // get all the aggregations that are referenced in chart, table and map and ensure they exist in the aggregations
+  const aggregationNames = config.aggregations.map(a => a.name)
 
-  const mapAggregations = config.applets.irs_monitor.map.aggregation_names
-  const tableAggregations = config.applets.irs_monitor.table.aggregation_names
-
-  const chartAggregationsArrays : ArrayLike<string | string[]> = config.applets.irs_monitor.charts
-    .filter(chartConfig => {
-      if (chartConfig.chart_type === 'text') {
-        return false
+  for (const aggregation of config.applets.irs_monitor.map.aggregation_names) {
+    if (!aggregationNames.includes(aggregation)){
+      return {
+        messages: [{ description: `The aggregation '${aggregation}' in map configuration is not in the aggregations`}],
+        status: EEdgeStatus.Yellow
       }
-
-      if (chartConfig.options.generate_series_from) {
-        return false
-      }
-
-      return true
-    })
-    .map(chartConfig => {
-      if (chartConfig.options.multi_series) {
-        return chartConfig.options.multi_series.map(m => m.aggregation_name)
-      }
-
-      if (chartConfig.options.single_series) {
-        return chartConfig.options.single_series.aggregation_name
-      }
-      
-      return []
-    })
-    
-
-  
-  const chartAggregations = flatten(chartAggregationsArrays)
-  const allAggregations = [...chartAggregations, ...mapAggregations, ...tableAggregations]
-
-  const definedAggregationStrings = config.aggregations.map(aggregation => aggregation.name)
-
-  const allExist = allAggregations.every(aggregationName => {
-    return definedAggregationStrings.includes(aggregationName)
-  })
-
-  if (!allExist) {
-    return {
-      messages: [{description: 'some missing'}],
-      status: EEdgeStatus.Yellow
     }
   }
 
+  for (const aggregation of config.applets.irs_monitor.table.aggregation_names) {
+    if (!aggregationNames.includes(aggregation)) {
+      return {
+        messages: [{ description: `The aggregation '${aggregation}' in table configuration is not in the aggregations` }],
+        status: EEdgeStatus.Yellow
+      }
+    }
+  }
+
+  // check they all exist in config.aggregations
+
+  const charts = config.applets.irs_monitor.charts.filter(chartConfig => {
+    if (chartConfig.chart_type === 'text') {
+      return false
+    }
+
+    if (chartConfig.options.generate_series_from) {
+      return false
+    }
+
+    return true
+  })
+    
+  for (const chart of charts) {
+    if (chart.options.multi_series) {
+      for (const series of chart.options.multi_series) {
+        if (!aggregationNames.includes(series.aggregation_name)) {
+          return {
+            messages: [{ description: `The aggregation '${series.aggregation_name}' in the chart configuration for ${chart.id} is not in the aggregations` }],
+            status: EEdgeStatus.Yellow
+          }
+        }
+      }
+    }
+    
+    if (chart.options.single_series) {
+      if (!aggregationNames.includes(chart.options.single_series.aggregation_name)) {
+        return {
+          messages: [{ description: `The aggregation '${chart.options.single_series.aggregation_name}' in the chart configuration for ${chart.id} is not in the aggregations` }],
+          status: EEdgeStatus.Yellow
+        }
+      }
+    }
+
+    // TODO: have to check pie charts
+  }
+  
   return {
     messages: [],
     status: EEdgeStatus.Green
