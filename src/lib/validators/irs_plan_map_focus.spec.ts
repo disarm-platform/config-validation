21c4519a400611486c22f5baec83df8ff6f1af0e
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-05-29 10:38:23 +0200

    Split functions

---
+++ src/lib/validators/irs_plan_map_focus.spec.ts
@@ -0,0 +1,33 @@
+// tslint:disable:no-expression-statement
+import {test} from 'ava';
+import {Config} from '../../definitions'
+import {EEdgeStatus , TEdgeResponse} from "../EdgeResponse";
+import {irs_plan_map_focus_validations} from "./irs_plan_map_focus";
+
+
+test('map focus not available', t => {
+  // @ts-ignore
+  const config: Config = {
+    "applets": {
+      "irs_plan": {}
+    }
+  }
+
+  const result: TEdgeResponse =  irs_plan_map_focus_validations(config);
+  t.is(result.status,EEdgeStatus.Blue)
+})
+
+test('map focus availble' , t =>{
+  // @ts-ignore
+  const config: Config = {
+    "applets": {
+      "irs_plan": {}
+    },
+    "map_focus": {
+      "zoom": 5
+    },
+  }
+
+  const result: TEdgeResponse =  irs_plan_map_focus_validations(config);
+  t.is(result.status,EEdgeStatus.Green)
+})
---
+++ src/lib/validators/irs_plan_spacial_hierarchy.spec.ts
@@ -0,0 +1,39 @@
+// tslint:disable:no-expression-statement
+import {test} from 'ava';
+import {Config} from '../../definitions'
+import {EEdgeStatus , TEdgeResponse} from "../EdgeResponse";
+import {irs_plan_spatial_hierarchy_validations} from "./irs_plan_spatial_hierarchy";
+
+
+test('planning_level  available', t => {
+  // @ts-ignore
+  const config: Config = {
+    "applets": {
+      "irs_plan": {}
+    },
+    "spatial_hierarchy": {
+      "markers": {
+        "planning_level_name": "village"
+      }
+    }
+  }
+
+  const result: TEdgeResponse =  irs_plan_spatial_hierarchy_validations(config);
+  t.is(result.status,EEdgeStatus.Green)
+})
+
+test('planing level available, map focus not availble' , t =>{
+  // @ts-ignore
+  const config: Config = {
+    "applets": {
+      "irs_plan": {}
+    },
+    "spatial_hierarchy": {
+      "markers": {
+      }
+    }
+  }
+
+  const result: TEdgeResponse =  irs_plan_spatial_hierarchy_validations(config);
+  t.is(result.status,EEdgeStatus.Yellow)
+})
