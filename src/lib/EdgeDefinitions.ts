6893ba8f31084c538974e3f75b95fb9ae224adad
Jonathan Smith <jonathan@peoplesized.com>
2018-06-04 09:28:34 +0200

    spell

--- src/lib/EdgeDefinitions.ts
+++ src/lib/EdgeDefinitions.ts
@@ -1,13 +1,13 @@
 // tslint:disable:object-literal-sort-keys
 
-export interface EdgeDefition {
+export interface EdgeDefinition {
   name: string;
   from: string;
   to: string;
 }
 
 
-export const EdgeDefitions : EdgeDefition[] = [
+export const EdgeDefinitions : EdgeDefinition[] = [
   {
     // do we need the function here as well? Seems nicer to have the actual function referenced, instead of doing it using strings
     name: 'irs_monitor_aggregations',
