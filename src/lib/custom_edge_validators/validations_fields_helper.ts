d38e6266ba67e3703bfd9b53255a7f47529c7cdf
Jonathan Smith <jonathan@peoplesized.com>
2018-06-04 21:21:53 +0200

    hmm

--- src/lib/custom_edge_validators/validations_fields_helper.ts
+++ src/lib/custom_edge_validators/validations_fields_helper.ts
@@ -1,42 +1,25 @@
-import { TConfig } from "../config_types/TConfig";
-import { ECustomEdgeStatus, TCustomEdgeResponse } from "../TCustomEdgeResponse";
-import { get_form_fields_for_validations } from "../helpers/expression_helpers";
-import { form_fields } from "../helpers";
+import { TConfig } from '../config_types/TConfig';
+import { form_fields, get_form_fields_for_validations } from '../helpers';
+import { ECustomEdgeStatus, TCustomEdgeResponse } from '../TCustomEdgeResponse';
 
 // checks that irs_record_point has the validations that it needs.
-// The function is named accordingly.
 export function validations_fields_helper(config: TConfig): TCustomEdgeResponse {
-  if (!config.validations) return {
-    messages: ['Missing validations - could be Blue too'],
-    status: ECustomEdgeStatus.Red
-  }
+  let messages: string[] = [];
+  let status = ECustomEdgeStatus.Red;
 
-  if (!config.form) return {
-    messages: ['Missing Form - could be Blue too'],
-    status: ECustomEdgeStatus.Red
-  }
-
-  if (!config.validations.length) {
-    return {
-      messages: [],
-      status: ECustomEdgeStatus.Blue
-    }
-  }
-
-  const formFields: string[] = form_fields(config.form)
-  const validationFields: string[] = get_form_fields_for_validations(config.validations)
+  const available_form_fields: string[] = form_fields(config.form);
+  const validationFields: string[] = get_form_fields_for_validations(config.validations);
 
   for (const field of validationFields) {
-    if (!formFields.includes(field)) {
+    if (!available_form_fields.includes(field)) {
       return {
-        messages: [`'${field}' not in form fields` ],
+        messages: [`'${field}' not in form fields`],
         status: ECustomEdgeStatus.Yellow
-      }
+      };
     }
   }
 
   return {
-    messages: [],
-    status: ECustomEdgeStatus.Green
-  }
+    messages, status
+  };
 }
--- src/lib/helpers/index.ts
+++ src/lib/helpers/index.ts
@@ -2,7 +2,7 @@ import { aggregations_field_helper } from './aggregations_field_helper';
 import { decorators_fields_helper } from './decorators_fields_helper';
 import { all_fields, decorator_fields } from './fields_helper';
 import { form_fields } from './form_helpers';
-import { expression_variables } from './expression_helpers';
+import { expression_variables, get_form_fields_for_validations } from './expression_helpers';
 
 export {
   aggregations_field_helper,
@@ -10,5 +10,6 @@ export {
   decorators_fields_helper,
   form_fields,
   decorator_fields,
-  expression_variables
+  expression_variables,
+  get_form_fields_for_validations
 };
