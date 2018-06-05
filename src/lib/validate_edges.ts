816705b1a9f1da6b422afef3d83df0bb0e3e98c2
Jonathan Smith <jonathan@peoplesized.com>
2018-06-05 14:26:16 +0200

    logic to determine edge response

--- src/lib/validate_edges.ts
+++ src/lib/validate_edges.ts
@@ -3,13 +3,14 @@ import { TConfig } from './config_types/TConfig';
 import custom_validations from './custom_edge_validators/index';
 import { create_helper_objects } from './helpers/index';
 import { TPathMap } from './helpers/path_mapping';
-import { ECustomEdgeStatus, TCustomEdgeResponse, TCustomEdgeResponses } from './TCustomEdgeResponse';
+import { ECustomEdgeStatus, TCustomEdgeResponses } from './TCustomEdgeResponse';
 import { TEdgeDefinition } from './TEdgeDefinition';
-import { EStandardEdgeStatus, TStandardEdgeResponses } from './TStandardEdgeResponse';
+import { EStandardEdgeStatus, TStandardEdgeResponse } from './TStandardEdgeResponse';
 import { THelpers } from './helpers/create_helper_objects';
 import { mapped_nodes, MappedNode } from './flatten_nodes';
+import { ENodeResponseStatus, TNodeResponse } from './TNodeResponse';
 
-export function validate_edges(config: TConfig, path_map: TPathMap[], edge_definitions: TEdgeDefinition[]): TStandardEdgeResponses {
+export function validate_edges(config: TConfig, path_map: TPathMap[], edge_definitions: TEdgeDefinition[]): TStandardEdgeResponse {
   // For every edge_definition
 
   // create helpers
@@ -21,7 +22,8 @@ export function validate_edges(config: TConfig, path_map: TPathMap[], edge_defin
   }));
 }
 
-function validate_edge(config: TConfig, nodes: MappedNode[], edge_definition: TEdgeDefinition, helpers: THelpers): TStandardEdgeResponses {
+function validate_edge(config: TConfig, nodes: MappedNode[], edge_definition: TEdgeDefinition, helpers: THelpers): TStandardEdgeResponse {
+  const node_responses = [];
   const response = [];
 
   // Basic checks for Node existence
@@ -35,20 +37,20 @@ function validate_edge(config: TConfig, nodes: MappedNode[], edge_definition: TE
 
   // if edge required and either nodes is missing => Red
   if (required && missing_a_node) {
-    response.push({
+    node_responses.push({
       message: `Required nodes (${source_node} & ${target_node}) are not found`,
       status: EStandardEdgeStatus.Red
     });
-    return response; // Return early, nothing else you can do
+    return determine_edge_result(edge_name, node_responses, required); // Return early, nothing else you can do
   }
 
   // if edge not required and missing either node => Blue
   if (!required && missing_a_node) {
-    response.push({
+    node_responses.push({
       message: 'Something missing, but edge not required anyway',
       status: EStandardEdgeStatus.Blue
     });
-    return response; // Return early, nothing else to do
+    return determine_edge_result(edge_name, node_responses, required); // Return early, nothing else to do
   }
 
   // Find and run the custom edge validation
@@ -70,3 +72,39 @@ function validate_edge(config: TConfig, nodes: MappedNode[], edge_definition: TE
   return response;
 }
 
+function determine_edge_result(edge_name: string, node_responses: TNodeResponse[], edge_required: boolean, custom_edge_responses?: TCustomEdgeResponses, ): TStandardEdgeResponse {
+  // Create a default response in case none of the other cases match.
+  let response = {edge_name, status: EStandardEdgeStatus.Red, message: 'Default response - not caught by any other cases'}
+
+  // All the tools
+  const edge_optional = !edge_required
+  const nodes_pass = node_responses.every(r => r.status === ENodeResponseStatus.Green)
+  const nodes_fail = !nodes_pass
+  const edge_passes = custom_edge_responses && custom_edge_responses.every(r => r.status === ECustomEdgeStatus.Green)
+  const edge_fails  = !edge_passes
+
+  // There are 3 binary variables (required, nodes pass, edge passes), so 8 cases
+  // 2 cases are dealt with by short-circuiting if nodes fail
+  // Leaves 6 cases to deal with explicitly
+  if (nodes_fail && edge_required) {
+    return {...response, status: EStandardEdgeStatus.Red, message: 'Failed - some missing node' }
+  }
+  if (nodes_fail && edge_optional) {
+    return {...response, status: EStandardEdgeStatus.Blue, message: 'One or more missing nodes, but edge not required' }
+  }
+  if (nodes_pass && edge_required && edge_passes) {
+    return {...response, status: EStandardEdgeStatus.Green, message: 'Required edge, nodes present and edge passes' }
+  }
+  if (nodes_pass && edge_optional && edge_passes) {
+    return {...response, status: EStandardEdgeStatus.Green, message: 'Optional edge, nodes present and edge passes' }
+  }
+  if (nodes_pass && edge_required && edge_fails) {
+    return {...response, status: EStandardEdgeStatus.Red, message: 'Required edge, nodes present and edge fails' }
+  }
+  if (nodes_pass && edge_optional && edge_fails) {
+    return {...response, status: EStandardEdgeStatus.Red, message: 'Optional edge, nodes present and edge fails' }
+  }
+
+  // Only used for fallthrough
+  return response
+}
