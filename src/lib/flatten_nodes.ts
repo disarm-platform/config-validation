2fb6ea801587383500576dfea3b0e8a13953c1bc
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-07 09:15:16 +0200

    Use MappedNode type from consistent-function-signatures branch

--- src/lib/flatten_nodes.ts
+++ src/lib/flatten_nodes.ts
@@ -1,10 +1,35 @@
-import {get} from 'lodash'
+import { get } from 'lodash'
+import { TAggregation } from './config_types/TAggregations';
+import { TApplet } from './config_types/TApplet';
 import { TConfig } from './config_types/TConfig';
+import { TDecorators } from './config_types/TDecorators';
+import { TForm } from './config_types/TForm';
+import { TInstance } from './config_types/TInstance';
+import { TIrsMonitor } from './config_types/TIrsMonitor';
+import { TIrsPlan } from './config_types/TIrsPlan';
+import { TIrsRecordPoint } from './config_types/TIrsRecordPoint';
+import { TIrsTasker } from './config_types/TIrsTasker';
+import { TLocationSelection } from './config_types/TLocationSelection';
+import { TMapFocus } from './config_types/TMapFocus';
+import { TSpatialHierarchy } from './config_types/TSpatialHierarchy';
+import { TValidation } from './config_types/TValidations';
 import { TPathMap } from './helper_functions/path_mapping';
 
 export interface MappedNode {
   name: string;
-  node: object;
+  node: TApplet &
+  TDecorators &
+  TAggregation[] &
+  TForm &
+  TInstance &
+  TIrsMonitor &
+  TIrsPlan &
+  TIrsRecordPoint &
+  TIrsTasker &
+  TLocationSelection &
+  TMapFocus &
+  TSpatialHierarchy &
+  TValidation[];
 }
 
 export function mapped_nodes(config: TConfig, path_map: TPathMap[]): MappedNode[] {
