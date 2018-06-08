22c1c9e48bae894175dda28d88953df64e0606b0
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-08 10:26:32 +0200

    that fixes it?

--- src/lib/custom_edge_validators/irs_monitor_aggregations.spec.ts
+++ src/lib/custom_edge_validators/irs_monitor_aggregations.spec.ts
@@ -1,36 +1,39 @@
 // tslint:disable:no-expression-statement
 import { test } from 'ava';
 import { TConfig } from '../config_types/TConfig';
+import { TIrsMonitor } from '../config_types/TIrsMonitor';
 import { ECustomEdgeStatus } from '../TCustomEdgeResponse';
 import { irs_monitor_aggregations } from './irs_monitor_aggregations';
 
 test('returns Red status if aggregation in map is not in aggregations', t => {
+  const irs_monitor: TIrsMonitor = {
+    charts: [],
+    map: {
+      aggregation_names: [
+        "number of rooms sprayed",
+        "room spray coverage (%)"
+      ],
+      bin_by: "location.selection.id",
+      chart_type: "map",
+      property_layers: [],
+      response_point_fields: []
+    },
+    season_start_dates: [],
+    table: {
+      aggregation_names: [],
+      bin_by: "location.selection.id",
+      chart_type: 'table',
+      property_layers: []
+    }
+  }
+
   const config = {
     aggregations: [{
       name: "number of rooms sprayed",
       numerator_expr: "number_sprayed"
     }],
     applets: {
-      irs_monitor: {
-        charts: [],
-        map: {
-          aggregation_names: [
-            "number of rooms sprayed",
-            "room spray coverage (%)"
-          ],
-          bin_by: "location.selection.id",
-          chart_type: "map",
-          property_layers: [],
-          response_point_fields: []
-        },
-        season_start_dates: [],
-        table: {
-          aggregation_names: [],
-          bin_by: "location.selection.id",
-          chart_type: 'table',
-          property_layers: []
-        }
-      }
+      irs_monitor
     }
   }
 
