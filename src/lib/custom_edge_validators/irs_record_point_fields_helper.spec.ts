257b51e7241c4a2dd6c751f9c86a72c53d311513
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-08 11:06:55 +0200

    Need to check that form exists. Might want to change edge definition instead.

--- src/lib/custom_edge_validators/irs_record_point_fields_helper.spec.ts
+++ src/lib/custom_edge_validators/irs_record_point_fields_helper.spec.ts
@@ -5,7 +5,13 @@ import { ECustomEdgeStatus } from "../TCustomEdgeResponse";
 import { irs_record_point_fields_helper } from './irs_record_point_fields_helper';
 
 test('should return Green status', t => {
-  const empty_object = {}
+  const empty_object = {form: {}}
   const result = irs_record_point_fields_helper(empty_object as TConfig)
   t.is(result[0].status, ECustomEdgeStatus.Green)
+})
+
+test('should return Red status', t => {
+  const empty_object = {}
+  const result = irs_record_point_fields_helper(empty_object as TConfig)
+  t.is(result[0].status, ECustomEdgeStatus.Red)
 })
