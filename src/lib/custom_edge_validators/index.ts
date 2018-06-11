ab8624482552076d6dc5d5e258d1340a405515a1
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-11 08:27:03 +0200

    Add custom edge validators back in

--- src/lib/custom_edge_validators/index.ts
+++ src/lib/custom_edge_validators/index.ts
@@ -2,18 +2,18 @@ import { TConfig } from '../config_types/TConfig';
 import { TCustomEdgeResponse } from '../TCustomEdgeResponse';
 import { aggregations_field_helper } from './aggregations_field_helper';
 import { decorators_form } from './decorators_form';
-// import { irs_monitor_aggregations } from './irs_monitor_aggregations';
+import { irs_monitor_aggregations } from './irs_monitor_aggregations';
 import { irs_monitor_fields_helper } from './irs_monitor_fields_helper';
+import { irs_monitor_map_focus } from './irs_monitor_map_focus';
 import { irs_monitor_spatial_hierarchy } from './irs_monitor_spatial_hierarchy';
-// import { irs_monitor_map_focus } from './irs_monitor_map_focus';
-// import { irs_plan_map_focus } from './irs_plan_map_focus'
+import { irs_plan_map_focus } from './irs_plan_map_focus'
 import { irs_plan_spatial_hierarchy } from './irs_plan_spatial_hierarchy'
 import { irs_record_point_fields_helper } from './irs_record_point_fields_helper';
 import { irs_record_point_location_selection } from './irs_record_point_location_selection';
-// import { irs_record_point_validations } from './irs_record_point_validations';
-// import { irs_tasker_map_focus } from './irs_tasker_map_focus';
-// import { irs_tasker_spatial_hierarchy } from './irs_tasker_spatial_hierarchy';
-// import { validations_fields_helper } from './validations_fields_helper';
+import { irs_record_point_validations } from './irs_record_point_validations';
+import { irs_tasker_map_focus } from './irs_tasker_map_focus';
+import { irs_tasker_spatial_hierarchy } from './irs_tasker_spatial_hierarchy';
+import { validations_fields_helper } from './validations_fields_helper';
 
 
 export interface TCustomEdgeValidators {
@@ -23,16 +23,16 @@ export interface TCustomEdgeValidators {
 export const CustomEdgeValidators: TCustomEdgeValidators = {
   aggregations_field_helper,
   decorators_form,
-  // irs_monitor_aggregations,
+  irs_monitor_aggregations,
   irs_monitor_fields_helper,
-  // irs_monitor_map_focus,
+  irs_monitor_map_focus,
   irs_monitor_spatial_hierarchy,
-  // irs_plan_map_focus,
+  irs_plan_map_focus,
   irs_plan_spatial_hierarchy,
   irs_record_point_fields_helper,
   irs_record_point_location_selection,
-  // irs_record_point_validations,
-  // irs_tasker_map_focus,
-  // irs_tasker_spatial_hierarchy,
-  // validations_fields_helper
+  irs_record_point_validations,
+  irs_tasker_map_focus,
+  irs_tasker_spatial_hierarchy,
+  validations_fields_helper
 };
