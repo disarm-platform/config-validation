c55a2edd06e34bbf30078b238728e8e54087b818
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-12 12:48:43 +0200

    Edge doesn’t exist anymore, add tests

--- src/lib/custom_edge_validators/irs_record_point_form.ts
+++ src/lib/custom_edge_validators/irs_record_point_form.ts
@@ -1,12 +1,7 @@
 import { TConfig } from "../config_types/TConfig";
 import { ECustomEdgeStatus, TCustomEdgeResponses } from "../TCustomEdgeResponse";
 
-export function irs_record_point_form(_config: TConfig) : TCustomEdgeResponses {
-  // TODO: check this approach. 
-  // irs_record_point relies on fields_helper which relies on form
-  // so this check shouldn't be necessary
-  // simpler would be to not have this edge, but rather irs_record_point_form
-  
+export function irs_record_point_form(_config: TConfig) : TCustomEdgeResponses {  
   return [{
     message: "Form, required by irs_record_point, is available.",
     status: ECustomEdgeStatus.Green
