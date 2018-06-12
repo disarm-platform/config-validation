6f81859f3de7129ad1f00944964ddb98650c607b
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-12 12:50:31 +0200

    Remove unused custom validator

--- src/lib/custom_edge_validators/index.ts
+++ src/lib/custom_edge_validators/index.ts
@@ -16,7 +16,6 @@ import { irs_monitor_map_focus } from './irs_monitor_map_focus';
 import { irs_monitor_spatial_hierarchy } from './irs_monitor_spatial_hierarchy';
 import { irs_plan_map_focus } from './irs_plan_map_focus'
 import { irs_plan_spatial_hierarchy } from './irs_plan_spatial_hierarchy'
-import { irs_record_point_fields_helper } from './irs_record_point_fields_helper';
 import { irs_record_point_form } from './irs_record_point_form'
 import { irs_record_point_location_selection } from './irs_record_point_location_selection';
 import { irs_record_point_validations } from './irs_record_point_validations';
@@ -49,7 +48,6 @@ export const CustomEdgeValidators: TCustomEdgeValidators = {
   irs_monitor_spatial_hierarchy,
   irs_plan_map_focus,
   irs_plan_spatial_hierarchy,
-  irs_record_point_fields_helper,
   irs_record_point_form,
   irs_record_point_location_selection,
   irs_record_point_validations,
