22e81a19b9a3f5d748e0b05edc2cf5fd0a4fed5f
Nicolai Davies <Nicolai.d@hotmail.com>
2018-07-09 09:51:04 +0200

    Test validator fails and passes

--- src/lib/custom_edge_validators/irs_record_point_form.spec.ts
+++ src/lib/custom_edge_validators/irs_record_point_form.spec.ts
@@ -5,13 +5,38 @@ import { ECustomEdgeStatus } from "../TCustomEdgeResponse";
 import { irs_record_point_form } from './irs_record_point_form';
 
 test('should return Green status', t => {
-  const config = {form: {}, applets:{}}
+  const config = {form: {}, applets:{ irs_record_point: {}}}
   const result = irs_record_point_form(config as TConfig)
   t.is(result[0].status, ECustomEdgeStatus.Green)
 })
 
-test('if filter_field is given, must exist on form', t => {
+test('if filter_field is given, must exist on form, return Red', t => {
   const config = {form: {}, applets:{irs_record_point: {filter_field: 'a'}}}
   const result = irs_record_point_form(config as TConfig)
   t.is(result[0].status, ECustomEdgeStatus.Red)
+})
+
+test('if filter_field is given, must exist on form, return green', t => {
+  const config = { 
+    applets: { irs_record_point: { filter_field: 'number_sprayable' } },
+    form: {
+      pages: [
+        {
+          elements: [
+            {
+              inputType: "number",
+              isRequired: true,
+              name: "number_sprayable",
+              title: "How many sprayable structures in the household/homestead?",
+              type: "text",
+            }
+          ],
+          name: "page1"
+        }
+      ]
+    } 
+  }
+  // @ts-ignore
+  const result = irs_record_point_form(config as TConfig)
+  t.is(result[0].status, ECustomEdgeStatus.Green)
 })
